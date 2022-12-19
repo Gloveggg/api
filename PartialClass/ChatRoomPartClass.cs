e76711128db7fc71d07031eda85cbefb4baa9c95
wsrvs@outlook.com <wsrvs@outlook.com>
2022-12-19 11:18:14 +0300

    Добавьте файлы проекта.

---
+++ PartialClass/ChatRoomPartClass.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebApplication2.Models;
+
+namespace WebApplication2.Models
+{
+    public partial class ChatRoom
+    {
+        public string GetGetLastMessage
+        {
+            get
+            {
+                string message = "Ничего нет";
+                try
+                {
+                    message = ChatMessage.ToList().LastOrDefault().textMessage;
+                    return message;
+                }
+                catch
+                {
+                    return message;
+                }
+            }
+        }
+    }
+}
